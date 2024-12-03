1d585e3f6e29c6c53ea2a1e600fa6115aee69ef1
nagumii <nagumi@gmail.com>
2024-12-03 09:09:24 +0200

    Merged in up to date scripts

--- foxbuntu/sysdrv/out/rootfs_uclibc_rv1106/usr/local/bin/wifi-mesh-control.sh
+++ foxbuntu/sysdrv/out/rootfs_uclibc_rv1106/usr/local/bin/wifi-mesh-control.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
-
-# Wait 60 seconds for networking to be available during boot
-sleep 60
-
-# Initial value for comparison (set to up by default)
-previous_value="1"
-
-# Ensure Wi-Fi interface is up by default on boot
 ip link set wlan0 up
+sleep 60 #wait 60 seconds so networking will be available during boot
+
+# Initial value for comparison (start with an impossible value so it'll always implement the current value after boot)
+previous_value=2
 
 while true; do
-    # Fetch the current value
+    # Run the command to fetch the value
     current_value=$(cat /root/.portduino/default/prefs/config.proto | protoc --decode_raw | awk '/4 {/, /}/ {if ($1 == "1:") print $2}')
 
     # Check if the value has changed
     if [ "$current_value" != "$previous_value" ]; then
-        # Log the previous state
-        case "$previous_value" in
-            1) prev_text="Wifi on" ;;
-            2) prev_text="Startup" ;;
-            *) prev_text="Wifi off" ;;
-        esac
 
-        # Log the current state
-        if [ "$current_value" -eq 1 ]; then
-            cur_text="Wifi on"
-        else
-            cur_text="Wifi off"
-        fi
+	if [ "$previous_value" -eq 1 ]; then
+	    prev_text="Wifi on"
+	elif [ "$previous_value" -eq 2 ]; then
+	    prev_text="Startup"
+	else
+	    prev_text="Wifi off"
+	fi
+	if [ "$current_value" -eq 1 ]; then
+	    cur_text="Wifi on"
+	else
+	    cur_text="Wifi off"
+	fi
 
         # Log the change
         logger "Wifi mesh control: wifi setting changed: $prev_text -> $cur_text"
@@ -36,10 +32,10 @@ while true; do
         previous_value="$current_value"
     fi
 
-    # Take action based on the current value
+    # Take action based on the new value
     if [ "$current_value" == "1" ]; then
         ip link set wlan0 up
-    elif [ "$current_value" == "0" ]; then
+    else
         ip link set wlan0 down
     fi
 
